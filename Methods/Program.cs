d42cfbfdf268064aa2fa2dcdc36061a0cfa9d444
cecilmartinez1989 <cecilmartineziii@ymail.com>
2019-07-02 17:05:28 -0500

    first

---
+++ Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+
+            
+                string name;
+                Console.WriteLine("Hi There!! What is your name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("OK! BYE BOB!");
+
+
+            
+            
+
+                int a;
+                int b;
+                int c;
+                string number, number2;
+                Console.WriteLine("Input your first number to add: ");
+                number = Console.ReadLine();
+                a = Convert.ToInt32(number);
+                Console.WriteLine("Now the second: ");
+                number2 = Console.ReadLine();
+                b = Convert.ToInt32(number2);
+                c = a + b;
+                Console.WriteLine("Your total is: {0}", c);
+
+
+                Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
---
+++ PigLatin/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pigword;
+            string pigfinal = "";
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            string[] vowels2 = new string[] { "a", "e", "i", "o", "u" };
+
+            Console.WriteLine("Type in your word to translate to piglatin: ");
+            pigword = Console.ReadLine();
+            string frist = pigword.Substring(0, 1);
+            string last = pigword.Substring(pigword.Length-1);
+            if (vowels2.Contains(frist)&&vowels2.Contains(last))
+            {
+                pigfinal = pigword + "yay";
+            }
+            else if(vowels2.Contains(frist)&& !vowels2.Contains(last))
+            {
+                pigfinal = pigword + "ay";
+            } 
+            else if (pigword.IndexOfAny(vowels)==-1)
+            {
+                pigfinal = pigword + "ay";
+
+
+            }
+            else if (!vowels2.Contains(frist) && pigword.IndexOfAny(vowels) > -1)
+            {
+                int index=pigword.IndexOfAny(vowels);
+                string before = pigword.Substring(0, index);
+                string after = pigword.Substring(index);
+                pigfinal = after + before + "ay";
+            }
+
+
+            Console.WriteLine(pigfinal);
+            Console.ReadLine();
+
+
+        }
+    }
+}
