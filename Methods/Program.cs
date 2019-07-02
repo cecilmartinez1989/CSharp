6fc91c8213d2b49d5cb0d2fb5e9dd058017ca333
cecilmartinez1989 <cecilmartineziii@ymail.com>
2019-07-02 18:29:43 -0500

    second

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -12,34 +12,129 @@ namespace Methods
             {
 
             
-                string name;
-                Console.WriteLine("Hi There!! What is your name: ");
-                name = Console.ReadLine();
-                Console.WriteLine("OK! BYE BOB!");
+                
 
 
-            
+           
             
 
-                int a;
-                int b;
-                int c;
-                string number, number2;
-                Console.WriteLine("Input your first number to add: ");
-                number = Console.ReadLine();
-                a = Convert.ToInt32(number);
-                Console.WriteLine("Now the second: ");
-                number2 = Console.ReadLine();
-                b = Convert.ToInt32(number2);
-                c = a + b;
-                Console.WriteLine("Your total is: {0}", c);
-
-
-                Console.ReadLine();
+              
 
 
 
             
         }
+        public static void hello()
+        {
+            string name;
+            Console.WriteLine("Hi There!! What is your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("OK! BYE " + name);
+        }
+        public static void add()
+        {
+            int a;
+            int b;
+            int c;
+            string number, number2;
+            Console.WriteLine("Input your first number to add: ");
+            number = Console.ReadLine();
+            a = Convert.ToInt32(number);
+            Console.WriteLine("Now the second: ");
+            number2 = Console.ReadLine();
+            b = Convert.ToInt32(number2);
+            c = a + b;
+            Console.WriteLine("Your total is: {0}", c);
+
+
+            Console.ReadLine();
+        }
+
+        public static void catdog()
+        {
+            string input;
+            Console.WriteLine("Do you prefer cats or dogs? ");
+            input = Console.ReadLine();
+            if (input == "cats")
+            {
+                Console.WriteLine("meow");
+            }
+            else if (input=="dogs")
+                {
+                Console.WriteLine("bark");
+            }
+        }
+        public static void oddEvent()
+        {
+            int x;
+            Console.WriteLine("enter a number:");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x % 2 == 1)
+            {
+                Console.WriteLine("odd");
+            }
+            else
+            {
+                Console.WriteLine("even");
+            }
+
+        }
+        public static void inches()
+        {
+            Console.WriteLine("Enter a amout in feet: ");
+
+            int feet= Convert.ToInt32(Console.ReadLine());
+
+            int inches = feet * 12;
+            Console.WriteLine(inches+" incehes");
+        }
+        public static void echo() {
+
+            Console.WriteLine("enter a word ");
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input.ToUpper());
+            Console.WriteLine(input.ToLower());
+            Console.WriteLine(input.ToLower());
+        }
+        
+        public static void kilograms()
+        {
+            Console.WriteLine("enter your wieght: ");
+            int pounds= Convert.ToInt32(Console.ReadLine());
+            double kilo = (double)pounds * 0.453592;
+
+            Console.WriteLine(kilo+ "in kilograms");
+
+
+
+        }
+        public static void date()
+        {
+            Console.WriteLine(DateTime.Today);
+        }
+        public static void age()
+        {
+            Console.WriteLine("What year where you born in? ");
+
+            int year= Convert.ToInt32(Console.ReadLine());
+            int age = DateTime.Today.Year - year;
+            Console.WriteLine("you are "+age);
+        }
+        public static void guess()
+        {
+            Console.WriteLine("guess a word ");
+            string input = Console.ReadLine();
+            if (input =="Csharp"){
+                Console.WriteLine("Correct");
+
+            }
+            else
+            {
+                Console.WriteLine("wrong");
+            }
+        }
+
     }
 }
