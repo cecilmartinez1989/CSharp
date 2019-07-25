a8ceae9dcbccc70cb13d148b06f24d2df8cc8899
cecilmartinez1989 <cecilmartineziii@ymail.com>
2019-07-25 17:42:20 -0500

    get

---
+++ SuperHero/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            person p2 = new person("Cecil", "C");
+            person p3 = new person("Donald", "Donny");
+            person s2 = new superhero("Tony Stark", "", "Iron Man", "Intelligence");
+            person s3 = new superhero("Peter Park", "", "Spider Man", "Spider Powers");
+            person v2 = new villian("Venom", "", "Spider Man");
+            person v3 = new villian("Thanos", "", "Avengers");
+
+            List<person> allperson = new List<person>();
+
+          
+
+        }
+        
+        class person
+        {
+            public string name;
+            public string nickname;
+
+            public person(string inname,string innickname)
+            {
+                this.name = inname;
+                this.nickname = innickname;
+            }
+            public void printgreeting()
+            {
+                person p1 = new person("William", "Bill");
+
+                Console.WriteLine("Hi, my name is {0}, you can call me {1}",p1.name,p1.nickname);
+
+                Console.ReadLine();
+            }
+            public override string ToString()
+            {
+                return name;
+            }
+
+        }
+
+        class superhero : person
+        {
+            public string realname;
+            public string superpower;
+            public superhero(string inname, string innickname, string irealname, string isuperpower) : base(inname, null) {
+
+                this.realname = irealname;
+                this.superpower = isuperpower;
+            }
+            public new void printgreeting()
+            {
+                superhero s1 = new superhero("Wade Turner","", "Mr. Incredible", "Super Strength");
+                Console.WriteLine("I am {0}. When I am {1}, my super power is {2}!",s1.name,s1.realname,s1.superpower);
+
+            }
+           
+        }
+
+        class villian : person
+        {
+            public string nemesis;
+            public villian(string inname, string innickname, string inemesis) : base(inname, null)
+            {
+                this.nemesis = inemesis;
+            }
+
+            public new void printgreeting()
+            {
+                villian v1 = new villian("Joker","","Batman");
+                Console.WriteLine("I am The {0}! Have you seen {1}?",v1.name,v1.nemesis);
+            }
+        }
+            
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,20 +8,158 @@ namespace TicTacToe
 {
     class Program
     {
+        public static string[,] board;
+        public static bool xturn = true;
+        public static string winner;
         static void Main(string[] args)
         {
+            board = new string[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = " ";
+                }
+            }
+
+            while (true)
+            { printboard();
+                Console.WriteLine("Enter the row and coloumn");
+                string input = Console.ReadLine();
+                string[] rawmove = input.Split(' ');
+                int[] move = new int[2];
+                move[0] = Convert.ToInt32(rawmove[0]) - 1;
+                move[1] = Convert.ToInt32(rawmove[1]) - 1;
+                placemark(move);
+                if (checkwin())
+                {
+                    break;
+                }
+            }
+            printboard();
+            Console.WriteLine("Winner is "+ winner);
+
+        }
+        public static void printboard()
+        {
+            Console.WriteLine("  1  2  3 ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(i + 1);
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"[{board[i, j]}]");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+        public static void placemark(int[] move)
+        {
+            if (board[move[0], move[1]] == " ")
+            {
+                if (xturn)
+                {
+                    board[move[0], move[1]] = "X";
+                }
+                else
+                {
+                    board[move[0], move[1]] = "O";
+                }
+                xturn = !xturn;
+            }
+
+        }
+        public static bool checkwin()
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                count = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == "X")
+                    {
+                        count++;
+                    }
+                    if (board[i, j] == "O")
+                    {
+                        count--;
+                    }
+                }
+                if (count == 3)
+                {
+                    winner = "X";
+                    return true;
+                }
+                if (count == -3)
+                {
+                    winner = "O";
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                count = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[j, i] == "X")
+                    {
+                        count++;
+                    }
+                    if (board[j, i] == "O")
+                    {
+                        count--;
+                    }
+                }
+                if (count == 3)
+                {
+                    winner = "X";
+                    return true;
+                }
+                if (count == -3)
+                {
+                    winner = "O";
+                    return true;
+                }
+            }
+            if(board[0,0]!= " "&& board[0,0]==board[1,1]&& board[1,1]==board[2,2])
+            {
+             if (board[1, 1] == "X")
+                {
+                    winner = "X";
+                    return true;
+                }
+                if (board[1, 1] == "O")
+                {
+                    winner = "O";
+                    return true;
+                }
+            }
+            if (board[0, 2] != " " && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[1, 1] == "X")
+                {
+                    winner = "X";
+                    return true;
+                }
+                if (board[1, 1] == "O")
+                {
+                    winner = "O";
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
 
             
-        }
-        public static void placeMark()
-        {
-
-        }
-        public static bool isHorizonalWin()
-        {
-
-        }
-
+    
+        
 
     }
 }
